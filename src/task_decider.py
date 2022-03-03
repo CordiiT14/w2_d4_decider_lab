26b6bb0a1c5140743e9f9db75219f9086a5978c8
CordiiT14 <cordelia.toennies@live.com>
2022-03-03 14:46:12 +0000

    added flipped tests for the extension. total 22 tests all passed

--- src/task_decider.py
+++ src/task_decider.py
@@ -1,7 +1,6 @@
 
 from src import task
 
-
 def task_decider(task1, task2):
     task_options = [task1.description, task2.description]
     if ("Wash Dishes" in task_options and "Cook Dinner" in task_options) or ("Wash Dishes" in task_options and "Wash Clothes" in task_options):
--- tests/test_task_decider.py
+++ tests/test_task_decider.py
@@ -44,21 +44,42 @@ class TestTaskDecider(unittest.TestCase):
 
     def test_dishes_over_clothes(self):
         self.assertEqual("Wash Dishes", task_decider(self.dishes, self.clothes))
+    
+    def test_dishes_over_clothes_flipped(self):
+        self.assertEqual("Wash Dishes", task_decider(self.clothes, self.dishes))
 
     def test_dinner_over_ironing(self):
         self.assertEqual("Cook Dinner", task_decider(self.dinner, self.ironing))
+    
+    def test_dinner_over_ironing_flipped(self):
+        self.assertEqual("Cook Dinner", task_decider(self.ironing, self.dinner))
 
     def test_windows_over_ironing(self):
         self.assertEqual("Clean Windows", task_decider(self.windows, self.ironing))
+    
+    def test_windows_over_ironing_flipped(self):
+        self.assertEqual("Clean Windows", task_decider(self.ironing, self.windows))
 
     def test_ironing_over_clothes(self):
         self.assertEqual("Do Ironing", task_decider(self.ironing, self.clothes))
+    
+    def test_ironing_over_clothes_flipped(self):
+        self.assertEqual("Do Ironing", task_decider(self.clothes, self.ironing))
 
     def test_ironing_over_dishes(self):
         self.assertEqual("Do Ironing", task_decider(self.ironing, self.dishes))
+    
+    def test_ironing_over_dishes_flipped(self):
+        self.assertEqual("Do Ironing", task_decider(self.dishes, self.ironing))
 
     def test_clothes_over_dinner(self):
         self.assertEqual("Wash Clothes", task_decider(self.clothes, self.dinner))
 
+    def test_clothes_over_dinner_flipped(self):
+        self.assertEqual("Wash Clothes", task_decider(self.dinner, self.clothes))
+
     def test_clothes_over_windows(self):
         self.assertEqual("Wash Clothes", task_decider(self.clothes, self.windows))
+    
+    def test_clothes_over_windows_flipped(self):
+        self.assertEqual("Wash Clothes", task_decider(self.windows, self.clothes))
