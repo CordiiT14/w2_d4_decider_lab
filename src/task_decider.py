ede88128a55294ed68160ae14a9aeccd9cf7017c
CordiiT14 <cordelia.toennies@live.com>
2022-03-03 14:10:25 +0000

    first extension test passed

--- src/task_decider.py
+++ src/task_decider.py
@@ -4,7 +4,7 @@ from src import task
 
 def task_decider(task1, task2):
     task_options = [task1.description, task2.description]
-    if "Wash Dishes" in task_options and "Cook Dinner" in task_options:
+    if ("Wash Dishes" in task_options and "Cook Dinner" in task_options) or ("Wash Dishes" in task_options and "Wash Clothes" in task_options):
         return "Wash Dishes"
     elif "Cook Dinner" in task_options and "Clean Windows" in task_options:
         return "Cook Dinner"
--- tests/test_task_decider.py
+++ tests/test_task_decider.py
@@ -9,6 +9,8 @@ class TestTaskDecider(unittest.TestCase):
         self.dishes = Task("Wash Dishes", 10)
         self.dinner = Task("Cook Dinner", 40)
         self.windows = Task("Clean Windows", 60)
+        self.clothes = Task("Wash Clothes", 50)
+        self.ironing = Task("Do Ironing", 45)
 
     def test_class_has_discription(self):
         self.assertEqual("Wash Dishes", self.dishes.description)
@@ -27,7 +29,6 @@ class TestTaskDecider(unittest.TestCase):
     def test_dinner_over_windows(self):
         self.assertEqual("Cook Dinner", task_decider(self.dinner, self.windows))
 
-     
     def test_dinner_over_windows_flipped(self):
         self.assertEqual("Cook Dinner", task_decider(self.windows, self.dinner))
 
@@ -38,3 +39,8 @@ class TestTaskDecider(unittest.TestCase):
     
     def test_windows_over_dishes_flipped(self):
         self.assertEqual("Clean Windows", task_decider(self.dishes, self.windows))
+
+# tests for extension
+
+    def test_dishes_over_clothes(self):
+        self.assertEqual("Wash Dishes", task_decider(self.dishes, self.clothes))
